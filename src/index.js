57d33e44257131e0975612c19219f4fe116c0959
yursha16 <yursha16@gmail.com>
2022-08-26 20:35:13 +0300

    Fixed index.js

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
   uri: "https://server-cat-5.herokuapp.com/", // change to YOUR own production server
   cache: new InMemoryCache(),
-  name: "web",
-  version: "1.0",
 });
 
 ReactDOM.render(
